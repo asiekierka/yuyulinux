fb90bedb2cbf7c1111486328e0c23ee9aade0ad5
asiekierka <kontakt@asie.pl>
2014-06-29 20:32:53 +0000

    libxft, xrdb

---
+++ xrdb/config.h
@@ -0,0 +1,4 @@
+#define _X_ATTRIBUTE_PRINTF(fmt,var) __attribute__((format(printf,fmt,var)))
+#define _X_NORETURN __attribute((noreturn))
+#define CPP "/bin/cpp"
+#define PACKAGE_STRING "xrdb-1.10-yuyulinux"
