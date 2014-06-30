1f3842fdb3bc0c3453739ab99ed2a8f7c6d3ac80
asiekierka <kontakt@asie.pl>
2014-06-30 09:17:24 +0000

    fix mdev, kernel; add dejavu and profont fonts

--- generate-repo.sh
+++ generate-repo.sh
@@ -2,6 +2,7 @@
 cd packages
 rm core.db
 rm core.db.tar.gz
-for i in *.pkg.tar.xz; do repo-add core.db.tar.gz $i; done
+#for i in *.pkg.tar.xz; do repo-add core.db.tar.gz $i; done
+repo-add core.db.tar.gz *
 rm core.db.tar.gz.old
 cd ..
